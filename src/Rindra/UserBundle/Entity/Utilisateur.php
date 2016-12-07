9698dcc04b46ec4768f9b6b67ae97c25d64f9715
rahrindra <rahrindra@gmail.com>
2016-12-07 15:30:26 +0300

    premier connexion réussi avec fosuser

---
+++ src/Rindra/UserBundle/Entity/Utilisateur.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rindra\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Utilisateur
+ *
+ * @ORM\Table(name="utilisateur")
+ * @ORM\Entity
+ */
+class Utilisateur extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
